fefeb8e0a48a35d4ed8fae4a7cf5f0fc20822ba1
Por7ableWorlD <universe.is.portable@gmail.com>
2023-11-05 02:40:53 +0400

    Added gitignore, clang-format and bat files

---
+++ build.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set ProjectName=CodeUE_Tips
+
+FOR /F "tokens=2* skip=2" %%a in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\EpicGames\Unreal Engine\5.3" /v "InstalledDirectory"') do set EnginePath=%%b
+
+if not exist "%EnginePath%" (
+	@echo "UE5.3 Is not installed. Unable to proceed"
+	pause
+	exit
+	)
+
+set VersionSelector=%EnginePath%
+set VersionSelector=%VersionSelector:\UE_5.3=%\Launcher\Engine\Binaries\Win64\UnrealVersionSelector.exe
+set UBTPath=%EnginePath%\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.exe
+set ProjectPath=%~dp0%ProjectName%.uproject
+
+@echo on
+echo Generate project files...
+"%VersionSelector%" -switchversionsilent "%ProjectPath%" "%EnginePath%"
+"%UBTPath%" -projectfiles -progress -project="%ProjectPath%"
+
+echo Building project...
+"%UBTPath%" %ProjectName%Editor Win64 Development "%ProjectPath%" 
+
+pause
